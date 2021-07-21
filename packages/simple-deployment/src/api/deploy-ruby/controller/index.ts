1003627ebefbf771c5f619d0aabc5ad1b777d8e1
Di Wu <di@gridx.cn>
2021-07-21 14:53:54 +0800

    Remove debug url.

--- packages/simple-deployment/src/api/deploy-ruby/controller/index.ts
+++ packages/simple-deployment/src/api/deploy-ruby/controller/index.ts
@@ -1,6 +1,5 @@
 import { GatewayRouterContext } from '../../../type';
 import { validator } from '../../../middleware';
-import axios from 'axios';
 
 export const showRubyServices = [
   validator(Schema => Schema.object({
@@ -8,7 +7,6 @@ export const showRubyServices = [
   })),
   async (context: GatewayRouterContext): Promise<void> => {
     await Promise.resolve();
-    await axios.get('http://akka-billing-utilitya.internal.gridx.com:8127/batch')
     context.gateway?.sendJSON?.({
       message: 'hello',
     });
