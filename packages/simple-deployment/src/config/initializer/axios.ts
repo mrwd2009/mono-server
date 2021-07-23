e2991c24e2da33a5cc5da78e875412be6aa01f1f
Di Wu <di@gridx.cn>
2021-07-23 09:05:42 +0800

    Add custom axios config.

--- packages/simple-deployment/src/config/initializer/axios.ts
+++ packages/simple-deployment/src/config/initializer/axios.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios, { AxiosError } from 'axios';
 import { BackendError } from '../../lib/error';
 
@@ -5,6 +6,11 @@ export const initialize = async (): Promise<void> => {
   axios.defaults.timeout = 60000 * 5;
   axios.defaults.headers.common['Content-Type'] = 'application/json';
   axios.interceptors.response.use((response) => {
+    const config: any = response.config;
+    // custom config field to return response object.
+    if (config.raw) {
+      return response;
+    }
     if (response.data?.meta?.code) {
       if (response.data.meta.code === 200) {
         return response.data.data;
