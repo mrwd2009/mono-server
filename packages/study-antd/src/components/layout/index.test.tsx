72b4401a7d5243649e8cda0399602f3403685cba
Di Wu <di@gridx.cn>
2022-01-19 08:34:29 +0800

    Add layout unit test.

--- packages/study-antd/src/components/layout/index.test.tsx
+++ packages/study-antd/src/components/layout/index.test.tsx
@@ -1,2 +1,16 @@
+import renderer from 'react-test-renderer';
 import Layout from './index';
 
+
+test('layout with full elements', () => {
+  const component = renderer.create((
+    <Layout>
+      <Layout.Sider>Sider</Layout.Sider>
+      <Layout.Content>
+        <Layout.Header>Header</Layout.Header>
+        <Layout.Footer>Footer</Layout.Footer>
+      </Layout.Content>
+    </Layout>
+  ));
+  expect(component.toJSON()).toMatchSnapshot();
+});
